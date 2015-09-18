511fcf8758d67a91bc01a8c3809b37b0b320ddf7
Devin Nusbaum <d.w.nusbaum@gmail.com>
2015-09-17 20:46:42 -0400

    Add more tests

--- hspec-tests.sh
+++ hspec-tests.sh
@@ -1,1 +1,1 @@
-runhaskell -package-db=.cabal-sandbox/x86_64-osx-ghc-7.8.3-packages.conf.d/ -isrc -itest test/Spec.hs
+runhaskell -package-db=.cabal-sandbox/x86_64-osx-ghc-7.8.3-packages.conf.d/ -itest test/Spec.hs
--- test/RegexSpec.hs
+++ test/RegexSpec.hs
@@ -7,9 +7,33 @@ import Regex
 spec :: Spec
 spec = do
   describe "Regex.matches" $ do
-    it "matches a string against a regex" $ do
-      let regex = Or (Symbol 'a') (Symbol 'b')
-      matches regex "a" `shouldBe` True
+    it "never matches if the regex is Nil" $
+      property $ \x -> matches Nil x `shouldBe` False
+
+    it "matches Empty against the empty string and nothing else" $
+      property $ \x ->
+        case x of
+          [] -> matches Empty x `shouldBe` True
+          _  -> matches Empty x `shouldBe` False
+
+    it "matches (Symbol c) against the single character c" $
+      property $ \x -> matches (Symbol x) [x] `shouldBe` True
+
+    it "matches (Or a b) against either a or b" $
+      property $ \x y ->
+        let regex = Or (Symbol x) (Symbol y)
+        in matches regex [x] && matches regex [y] `shouldBe` True
+
+    it "matches (Seq a b) against a followed by b" $
+      property $ \x y ->
+        let regex = Seq (Symbol x) (Symbol y)
+        in matches regex [x, y] `shouldBe` True
+
+    it "matches (Kleene r) against r zero or more times" $
+      property $ \x i ->
+        let regex = Kleene (Symbol x)
+        in matches regex "" && matches regex (replicate i x) `shouldBe` True
+
 
 main :: IO ()
 main = hspec spec
