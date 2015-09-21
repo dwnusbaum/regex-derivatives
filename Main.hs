a291ce07e70ecd6eb277bab04cdb93bf68e08770
Devin Nusbaum <d.w.nusbaum@gmail.com>
2015-09-21 14:51:26 -0400

    Display all matches on the same line of the string in the same line of the output

--- Main.hs
+++ Main.hs
@@ -5,17 +5,28 @@ import System.Environment
 import Parse
 import Regex
 
--- | Pretty prints a single match of a string
-showMatch :: String -> Match -> String
-showMatch str match = unlines [str, frontPadding (start match) ++ matchString (size match)]
+-- | Returns a two line string, where the first line is the original text and
+-- the second line underlines the matching parts
+showMatchesLine :: String -> [Match] -> String
+showMatchesLine _ [] = ""
+showMatchesLine s ms = unlines [s, underlineMatches ms]
+
+-- | Returns a string with a carat (^) in the location of each match start and
+--  tildes (~) underlining the rest of the match.
+-- i.e. showMatchesLine [Match 0 2, Match 3 3] would return "^~ ^~~"
+underlineMatches :: [Match] -> String
+underlineMatches ms = fst $ foldl underline ("", 0) ms
   where
-    frontPadding n = replicate n ' '
-    matchString n = take n $ '^' : replicate (n - 1) '~'
+    underline (str, strLen) (Match mStart mSize) = (str ++ padMatch ++ displayMatch, strLen')
+      where
+        strLen' = mStart - strLen + mSize
+        padMatch = replicate (mStart - strLen) ' '
+        displayMatch = take mSize $ '^' : replicate (mSize - 1) '~'
 
 -- | Given a regex and a string, this function returns a pretty printed list
--- of strings that show where the regex matched.
-showMatches :: Regex -> String -> [String]
-showMatches r s = map (showMatch s) $ allMatches r s
+-- of strings that show where the regex matched the string.
+showAllMatches :: Regex -> String -> [String]
+showAllMatches r s = filter (not . null) $ map (\s' -> showMatchesLine s' $ allMatches r s') $ lines s
 
 main :: IO ()
 main = do
@@ -23,6 +34,6 @@ main = do
     case length args of
         2 -> case parseRegex $ head args of
                  Left  err   -> print err
-                 Right regex -> putStr $ unlines $ showMatches regex $ args !! 1
+                 Right regex -> putStr $ concat $ showAllMatches regex $ args !! 1
         _ -> putStrLn "Wrong number of arguments. Try: regex a* aaaa"
 
