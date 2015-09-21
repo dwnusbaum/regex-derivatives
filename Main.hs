403ea47cdc5022d5335f4fc3cf7187647d711e01
Devin Nusbaum <d.w.nusbaum@gmail.com>
2015-09-21 15:52:39 -0400

    Highlight matches in the output of Main.main

--- Main.hs
+++ Main.hs
@@ -1,19 +1,44 @@
 module Main where
 
-import System.Environment
+import qualified System.Console.ANSI as ANSI
+import           System.Environment  (getArgs)
 
 import Parse
 import Regex
 
--- | Returns a two line string, where the first line is the original text and
+-- | This string, if output to a UNIX terminal, will reset the terminal to its default display
+resetSGRCode :: String
+resetSGRCode = ANSI.setSGRCode [ANSI.Reset]
+
+-- | Returns a two line string, where the first line contains the original text and
 -- the second line underlines the matching parts
-showMatchesLine :: String -> [Match] -> String
-showMatchesLine _ [] = ""
-showMatchesLine s ms = unlines [s, underlineMatches ms]
+showMatchesLine :: String -> Int -> [Match] -> String
+showMatchesLine _ _ [] = ""
+showMatchesLine s i ms = prefix ++ highlightMatches s ms
+  where prefix = ANSI.setSGRCode prefixCode ++ show i ++ resetSGRCode ++ ":"
+        prefixCode = [ ANSI.SetColor ANSI.Foreground ANSI.Vivid ANSI.Yellow
+                     , ANSI.SetConsoleIntensity ANSI.BoldIntensity
+                     ]
+
+-- | Highlights all of the matches on a particular line using ANSI SGR codes
+highlightMatches :: String -> [Match] -> String
+highlightMatches s = go s . relative 0
+  where
+    go str []     = str
+    go str (m:ms) = before ++ matchSGRCode ++ match ++ resetSGRCode ++ highlightMatches after ms
+      where
+        (before, matchAndAfter) = splitAt (start m) str
+        (match, after) = splitAt (size m) matchAndAfter
+        matchSGRCode = ANSI.setSGRCode [ ANSI.SetColor ANSI.Background ANSI.Dull ANSI.Green
+                                       , ANSI.SetColor ANSI.Foreground ANSI.Dull ANSI.Black
+                                       ]
+    relative _ []     = []
+    relative i (m:ms) = m { start = start m - i } : relative (start m - i + size m) ms
 
 -- | Returns a string with a carat (^) in the location of each match start and
---  tildes (~) underlining the rest of the match.
--- i.e. showMatchesLine [Match 0 2, Match 3 3] would return "^~ ^~~"
+--  tildes (~) underlining the rest of the match.  This function assumes that
+-- the string is will be displayed under also starts at index 0 on the display
+-- i.e. underlineMatches [Match 0 2, Match 3 3] would return "^~ ^~~".
 underlineMatches :: [Match] -> String
 underlineMatches ms = fst $ foldl underline ("", 0) ms
   where
@@ -26,7 +51,7 @@ underlineMatches ms = fst $ foldl underline ("", 0) ms
 -- | Given a regex and a string, this function returns a pretty printed list
 -- of strings that show where the regex matched the string.
 showAllMatches :: Regex -> String -> [String]
-showAllMatches r s = filter (not . null) $ map (\s' -> showMatchesLine s' $ allMatches r s') $ lines s
+showAllMatches r s = filter (not . null) $ map (\(s', i) -> showMatchesLine s' i $ allMatches r s') $ zip (lines s) [1..]
 
 main :: IO ()
 main = do
@@ -34,6 +59,6 @@ main = do
     case length args of
         2 -> case parseRegex $ head args of
                  Left  err   -> print err
-                 Right regex -> putStr $ concat $ showAllMatches regex $ args !! 1
+                 Right regex -> putStr $ unlines $ showAllMatches regex $ args !! 1
         _ -> putStrLn "Wrong number of arguments. Try: regex a* aaaa"
 
