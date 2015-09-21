fcac5a932b52a0b752e1bd3fe0e6fda0fe14c5e3
Devin Nusbaum <d.w.nusbaum@gmail.com>
2015-09-21 13:58:33 -0400

    Update main function
    Main.main now pretty prints all matches found

--- Main.hs
+++ Main.hs
@@ -5,12 +5,24 @@ import System.Environment
 import Parse
 import Regex
 
+-- | Pretty prints a single match of a string
+showMatch :: String -> Match -> String
+showMatch str match = unlines [str, frontPadding (start match) ++ matchString (size match)]
+  where
+    frontPadding n = replicate n ' '
+    matchString n = take n $ '^' : replicate (n - 1) '~'
+
+-- | Given a regex and a string, this function returns a pretty printed list
+-- of strings that show where the regex matched.
+showMatches :: Regex -> String -> [String]
+showMatches r s = map (showMatch s) $ allMatches r s
+
 main :: IO ()
 main = do
     args <- getArgs
     case length args of
         2 -> case parseRegex $ head args of
-               Left  err -> print err
-               Right reg -> print $ matches reg $ head $ tail args
+                 Left  err   -> print err
+                 Right regex -> putStr $ unlines $ showMatches regex $ args !! 1
         _ -> putStrLn "Wrong number of arguments. Try: regex a* aaaa"
 
